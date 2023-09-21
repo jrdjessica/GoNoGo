d2ff519369ddb04d3eceebb57fcace52da5e0b88
Jessica Dea <jessicardea@gmail.com>
2023-09-20 19:15:56 -0700

    Removed create_user function

--- GoNoGo/authentication/models.py
+++ GoNoGo/authentication/models.py
@@ -1,20 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, first_name=None, last_name=None):
-        email = email.lower()
-
-        user = self.model(
-            email=email,
-            password=password,
-            first_name=first_name,
-            last_name=last_name
-        )
-
-        user.save(using=self._db)
-        return user
 
 # User Model
 class User(models.Model):
