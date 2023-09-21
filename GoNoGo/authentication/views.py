c5126332558ab649e5d24871c6d19bd988fed676
Jessica Dea <jessicardea@gmail.com>
2023-09-20 19:15:10 -0700

    Removed old signup_view

--- GoNoGo/authentication/views.py
+++ GoNoGo/authentication/views.py
@@ -67,35 +67,3 @@ def signup_view(request):
 class Users(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-# @csrf_exempt
-# def signup_view(request):
-#     if request.method == 'POST':
-#         # Parse the JSON payload
-#         data = json.loads(request.body.decode('utf-8'))
-
-#         first_name = data.get('first_name')
-#         last_name = data.get('last_name')
-#         email = data.get('email')
-#         password = data.get('password')
-
-#         # Validate the data (you can add more robust validation here)
-#         if not all([first_name, last_name, email, password]):
-#             return JsonResponse({'message': 'Invalid data'}, status=400)
-
-#         # Check if user already exists
-#         if User.objects.filter(email=email).exists():
-#             return JsonResponse({'message': 'Email already exists'}, status=400)
-
-#         # Create a new user
-#         user = User.objects.create_user(
-#             email=email,
-#             password=password,
-#             first_name=first_name,
-#             last_name=last_name
-#         )
-#         user.save()
-
-#         return JsonResponse({'message': 'Account created successfully'}, status=201)
-
-#     return JsonResponse({'message': 'Only POST method is allowed'}, status=405)
