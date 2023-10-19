730327ebef54ce385869efd00b355e392f7fcc4e
emmacodes <binma.emma@gmail.com>
2023-10-18 17:41:04 -0700

    make the close modal btn and return to dashboard

--- GoNoGo/static/js/main.js
+++ GoNoGo/static/js/main.js
@@ -1,4 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    alert("Hello, Django!");
+    const newEventBtn = document.getElementById('new-event-btn');
+    const newEventModal = document.getElementById('new-event-modal');
+    const newEventModalContent = document.getElementById('new-event-modal-content');
+
+    function attachCloseEvent() {
+        const closeBtn = document.getElementById('close-btn'); // Look for closeBtn after content is loaded
+        if (closeBtn) {
+            closeBtn.addEventListener('click', function () {
+                console.log("close btn!!!!!!");
+                newEventModal.style.display = 'none'; // Hide the form
+                window.location.href = '/dashboard/'; // Redirect to dashboard
+            });
+        }
+    }
+
+    newEventBtn.addEventListener('click', function () {
+        fetch(`/new_event/`)
+            .then(response => response.text())
+            .then(data => {
+                newEventModalContent.innerHTML = data;
+                newEventModal.style.display = 'block';
+                window.history.pushState({}, null, '/new_event/');
+                attachCloseEvent();  // Call function to attach the close button event
+            });
+    });
 });
+
+
 
