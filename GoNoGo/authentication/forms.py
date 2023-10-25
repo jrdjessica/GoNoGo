1cf8f0e6e2f6ea120bcfcf448dee82e181e5147a
emmacodes <binma.emma@gmail.com>
2023-10-24 17:46:10 -0700

    styled log in page

--- GoNoGo/authentication/forms.py
+++ GoNoGo/authentication/forms.py
@@ -7,6 +7,10 @@ class LogInForm(forms.Form):
 
     email = forms.EmailField(required=True)
     password = forms.CharField(widget=forms.PasswordInput(), required=True)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({'placeholder': 'Your Email'})
+        self.fields['password'].widget.attrs.update({'placeholder': 'Your Password'})
 
 # Sign Up Form
 
