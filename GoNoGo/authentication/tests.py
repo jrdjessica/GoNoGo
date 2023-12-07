6af9fb8efd019aa21e414b00faffc7da611ff917
Jessica Dea <jessicardea@gmail.com>
2023-12-06 16:34:56 -0800

    Created event tests

--- GoNoGo/authentication/tests.py
+++ GoNoGo/authentication/tests.py
@@ -1,31 +1,28 @@
 from django.test import TestCase
 from .models import User
 from .forms import LogInForm
+
+
 class UserTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Set up non-modified objects used by all test methods
-        User.objects.create(first_name='Big', last_name='Bob', email='anyemail@gmail.com', password='1234455555' )
+        User.objects.create(first_name='Big', last_name='Bob',
+                            email='anyemail@gmail.com', password='1234455555')
 
     def test_first_name_label(self):
         user = User.objects.get(id=1)
         field_label = User._meta.get_field('first_name').verbose_name
         self.assertEqual(field_label, 'first name')
 
-
     def test_first_name_max_length(self):
         user = User.objects.get(id=1)
         max_length = User._meta.get_field('first_name').max_length
         self.assertEqual(max_length, 50)
 
-    # def test_object_name_is_last_name_comma_first_name(self):
-    #     User = User.objects.get(id=1)
-    #     expected_object_name = f'{User.last_name}, {User.first_name}'
-    #     self.assertEqual(str(User), expected_object_name)
 
-  
 class LoginFormTest(TestCase):
-   
+
     def test_valid_data(self):
         form = LogInForm(data={
             'email': 'user@example.com',
@@ -40,7 +37,10 @@ class LoginFormTest(TestCase):
             'email': ['This field is required.'],
             'password': ['This field is required.'],
         })
+
     def test_field_placeholder(self):
         form = LogInForm()
-        self.assertEqual(form.fields['email'].widget.attrs['placeholder'], 'Your Email')
-        self.assertEqual(form.fields['password'].widget.attrs['placeholder'], 'Your Password')
+        self.assertEqual(
+            form.fields['email'].widget.attrs['placeholder'], 'Your Email')
+        self.assertEqual(
+            form.fields['password'].widget.attrs['placeholder'], 'Your Password')
--- GoNoGo/events/tests.py
+++ GoNoGo/events/tests.py
@@ -1,3 +1,100 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Event
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class EventTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        User.objects.create(username='organizer_user')
+        User.objects.create(username='attendee1')
+        User.objects.create(username='attendee2')
+
+    def setUp(self):
+        # Set up modified objects used by individual test methods
+        organizer = User.objects.get(username='organizer_user')
+        event = Event.objects.create(
+            id=1,
+            title='Test Event',
+            content='Test content',
+            location='Test location',
+            date='2024-01-01',
+            time='12:00:00',
+            decision=True,
+            organizer=organizer
+        )
+        attendee1 = User.objects.get(username='attendee1')
+        attendee2 = User.objects.get(username='attendee2')
+        event.attendees.add(attendee1)
+
+    def test_attendees_added(self):
+        event = Event.objects.get(id=1)
+        attendee1 = User.objects.get(username='attendee1')
+        self.assertIn(attendee1, event.attendees.all())
+
+    def test_attendees_count(self):
+        event = Event.objects.get(id=1)
+        self.assertEqual(event.attendees.count(), 1)
+
+    def test_add_attendee(self):
+        event = Event.objects.get(id=1)
+        attendee2 = User.objects.get(username='attendee2')
+        event.attendees.add(attendee2)
+        self.assertIn(attendee2, event.attendees.all())
+        self.assertEqual(event.attendees.count(), 2)
+
+
+class EditEventViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        User.objects.create(username='organizer_user')
+        User.objects.create(username='attendee1')
+
+    def setUp(cls):
+        organizer = User.objects.get(username='organizer_user')
+        event = Event.objects.create(
+            id=2,
+            title='Test Event',
+            content='Test content',
+            location='Test location',
+            date='2024-01-01',
+            time='12:00:00',
+            decision=True,
+            organizer=organizer
+        )
+        attendee1 = User.objects.get(username='attendee1')
+        event.attendees.add(attendee1)
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/dashboard/event/2/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('edit_event', args=[2]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('edit_event', args=[2]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'edit.html')
+
+    def test_past_date_event_create_error(self):
+        organizer = User.objects.get(username='organizer_user')
+        attendee1 = User.objects.get(username='attendee1')
+        post_data = {
+            'title': 'Test Event',
+            'content': 'Test content',
+            'location': 'Test location',
+            'date': '2020-01-01',
+            'time': '12:00:00',
+            'decision': True,
+            'organizer': organizer.id,
+            'attendees': [attendee1.id],
+        }
+        response = self.client.post(reverse('new_event'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(
+            response, 'form', None, 'The event time is in the past. Please choose a future date and time.')
